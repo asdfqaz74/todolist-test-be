57566dd170b40c8a6d8b787c9ed90b5a4c0b3667
KangJS <asdfqaz1996@naver.com>
2024-10-21 16:09:35 +0900

    Refactor JWT_SECRET_KEY in User model to use environment variable

--- model/User.js
+++ model/User.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 const jwt = require("jsonwebtoken");
 require("dotenv").config();
-const JWT_SECRET_KEY = process.env.JWT_SECRET_KEY;
+const JWT_SECRET_KEY = `${process.env.JWT_SECRET_KEY}`;
 
 // User 스키마를 정의합니다.
 const UserSchema = new Schema(
